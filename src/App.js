e7198a1751ac3079ab7aa55a1dc38da2ce217337
AkshayEkunde <akshayekunde2000@gmail.com>
2025-03-14 15:49:34 +0530

    Initial React App

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css"; // Import CSS file
+
+function App() {
+  return (
+    <div className="iframe-container">
+      <iframe
+        src="https://akshay.qodeways.com/"
+        title="Embedded Page"
+      ></iframe>
+    </div>
+  );
+}
+
+export default App;
